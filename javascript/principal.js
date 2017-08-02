3c2ce254a0a221f40641af4fbc8d229cdf78a7f4
cust6792 <cust6792@caelumBSB01-06>
2017-08-02 15:18:25 -0300

    Aula 3 WebApp responsivo

---
+++ javascript/principal.js
@@ -0,0 +1,57 @@
+var mural = document.querySelector('.mural');
+
+function efetuaMudancaDeLayout() {
+    
+    mural.classList.toggle('mural--linhas');
+    
+}
+
+var botaoLayout = document.querySelector('#mudaLayout');
+//botaoLayout.onclick = efetuaMudancaDeLayout;
+
+botaoLayout.addEventListener('click', efetuaMudancaDeLayout);
+
+botaoLayout.addEventListener('click', function() {
+    if (mural.classList.contains('mural--linhas')) {
+        botaoLayout.textContent = 'Blocos';
+    } else {
+        botaoLayout.textContent = 'Linhas';
+    }
+});
+
+var botoesRemove = document.querySelectorAll('.opcoesDoCartao-remove');
+for (var i=0; i<botoesRemove.length; i++){
+    var botaoRemove = botoesRemove[i];
+    botaoRemove.addEventListener('click', removeCartao);
+}
+
+function removeCartao(){
+    var idDoCartao = this.dataset.cartao;
+    var cartao = document.querySelector('#cartao' + idDoCartao);
+    cartao.classList.add('cartao--some');
+    setTimeout(function() {
+        cartao.remove();
+    }, 2000);
+    
+}
+
+var formulario = document.querySelector('.novoCartao');
+formulario.addEventListener('submit', salvaCartao);
+
+function salvaCartao(evento){
+    evento.preventDefault();
+    var campoConteudo = document.querySelector('.novoCartao-conteudo');
+    var digitado = campoConteudo.value;
+    console.log(digitado);
+    
+    var conteudoNovoCartao = document.createElement('p');
+    conteudoNovoCartao.classList.add('cartao-conteudo');
+    conteudoNovoCartao.textContent = digitado;
+    
+    var novoCartao = document.createElement('div');
+    novoCartao.classList.add('cartao');
+    novoCartao.appendChild(conteudoNovoCartao);
+    
+    var mural = document.querySelector('.mural');
+    mural.insertBefore(novoCartao, mural.firstElementChild);
+}
