aae3b79072807f6a06e41e039bd4f27790ca7a63
Glauter Santos <glauterlima@gmail.com>
2017-08-03 17:30:44 -0300

    capitulo 5 finalizado

--- javascript/principal.js
+++ javascript/principal.js
@@ -84,6 +84,15 @@ function decideTipoCartao(texto) {
     }
 }
 
+$('#busca').on('input', function() {
+    var digitado = $(this).val().trim();
+    var regex = new RegExp(digitado, 'i');
+    $('.cartao').hide().filter(function() {
+        var conteudo = $(this).find('.cartao-conteudo').text();
+        return regex.test(conteudo);
+    }).show();
+});
+
 
 /*function salvaCartao(evento){
     evento.preventDefault();
